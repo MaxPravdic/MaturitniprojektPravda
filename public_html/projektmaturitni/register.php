b116ab020c8421f02497f7a2fcb3466948e15434
MaxPravdic <maxpravda@email.cz>
2025-08-04 11:30:26 +0200

    Update register.php

--- public_html/projektmaturitni/register.php
+++ public_html/projektmaturitni/register.php
@@ -4,10 +4,9 @@ $username = "student18";
 $password = "spsnet";
 $database = "vyuka18";
 
-// Vytvoření připojení
+
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Kontrola připojení
 if ($conn->connect_error) {
     die("Chyba připojení: " . $conn->connect_error);
 }
@@ -18,17 +17,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $heslo = $_POST['password'];
         $spz = $_POST['spz'];
 
-        // Hashování hesla pro bezpečnost
+       
         $hashed_heslo = password_hash($heslo, PASSWORD_DEFAULT);
 
-        // Připravení SQL dotazu pro vložení dat
+        
         $sql = "INSERT INTO uzivatele (uzivatelske_jmeno, heslo, spz) VALUES (?, ?, ?)";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("sss", $uzivatelske_jmeno, $hashed_heslo, $spz);
 
         if ($stmt->execute()) {
             
-            // Přesměrování zpět na stránku rezervací
+          
             header("Location: https://xeon.spskladno.cz/~pravdam/projekt%20maturitn%C3%AD/pologinu.php");
             exit();
         } else {
@@ -41,3 +40,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $conn->close();
 ?>
+
